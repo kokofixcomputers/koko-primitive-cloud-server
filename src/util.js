4910119a84b8dd74ffe4c934f164df22eaf75bc4
kokofixcomputers <github@kokofixcomputers.serv00.net>
2024-01-23 16:59:19 -0800

    add comment

--- src/util.js
+++ src/util.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 
+//Some functions to not duplicate code.
+
 module.exports.readFile = filePath => {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
