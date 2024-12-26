3025c8eb5a14334570118bc351ad08bc3c8b3a99
kokodev <github@kokofixcomputers.serv00.net>
2024-12-26 11:52:24 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,69 +1,54 @@
-const path = require("path");
-const publicIp = require("public-ip");
-const internalIp = require("internal-ip");
-const express = require("express");
-const expressWs = require("express-ws");
-const colours = require("colors/safe");
+const path = require('path')
+const publicIp = require('public-ip')
+const internalIp = require('internal-ip')
+const express = require('express')
+const expressWs = require('express-ws')
+const colours = require('colors/safe')
 
-const CloudServer = require("./cloud-server.js");
-const fsUtil = require("./util.js");
+const CloudServer = require('./cloud-server.js')
+const fsUtil = require('./util.js')
 
-async function startServer({ port, lockVars, perMessageDeflate }) {
-  const app = express();
-  const cloudServer = new CloudServer({ lockVars });
+async function startServer ({ port, lockVars, perMessageDeflate }) {
+  const app = express()
+  const cloudServer = new CloudServer({ lockVars })
 
-  app.disable("x-powered-by");
+  app.disable('x-powered-by')
   expressWs(app, undefined, {
-    wsOptions: { perMessageDeflate },
-  });
+    wsOptions: { perMessageDeflate }
+  })
 
-  const oldIndexHtmlPath = path.resolve(__dirname, "../index.html");
+  const oldIndexHtmlPath = path.resolve(__dirname, '../index.html')
   if (await fsUtil.exists(oldIndexHtmlPath)) {
-    app.get("/", (req, res, next) => {
-      res.sendFile(oldIndexHtmlPath);
-    });
+    app.get('/', (req, res, next) => {
+      res.sendFile(oldIndexHtmlPath)
+    })
   }
 
-  app.use(
-    express.static(path.resolve(__dirname, "../static/"), {
-      extensions: ["html", "htm"],
-    }),
-  );
+  app.use(express.static(path.resolve(__dirname, '../static/'), {
+    extensions: ['html', 'htm']
+  }))
 
-  //Handle the connection using another file if protocal is websocket (wss)
-  app.ws("/", cloudServer.handleWsConnection);
+  app.ws('/', cloudServer.handleWsConnection)
 
   app.use((req, res) => {
-    res.status(404).sendFile(path.resolve(__dirname, "../static/404.html"));
-    //If page not found, return 404 to aviod errors and misunderstanding
-  });
+    res.status(404).sendFile(path.resolve(__dirname, '../static/404.html'))
+  })
 
   app.listen(port, async () => {
-    //Log info
-    console.log(colours.green("Your server is now running!"));
-    console.log("You can access it...");
-    console.log(
-      `  • on your computer at ${colours.cyan(`ws://localhost:${port}/`)} (use this for testing)`,
-    );
-    const privateIp = await internalIp.v4().catch(() => null);
+    console.log('Please consider donating to kokofixcomputers by going to github sponsors. Any amount is appreciated!')
+    console.log(colours.green('Scratch Primitive Cloud Server is now running:'))
+    console.log(`  • on your computer at ${colours.cyan(`ws://localhost:${port}/`)} (use this for testing)`)
+    const privateIp = await internalIp.v4().catch(() => null)
     if (privateIp) {
-      console.log(
-        `  • locally within your network at ${colours.blue(`ws://${privateIp}:${port}/`)} (maybe)`,
-      );
+      console.log(`  • locally within your network at ${colours.blue(`ws://${privateIp}:${port}/`)} (This may be incorrect.)`)
     }
-    const ip = await publicIp.v4().catch(() => null);
+    const ip = await publicIp.v4().catch(() => null)
     if (ip) {
-      console.log(
-        `  • publicly at ${colours.blue(`ws://${ip}:${port}/`)}, but ONLY if you've set up port forwarding on your router`,
-      );
+      console.log(`  • publicly at ${colours.blue(`ws://${ip}:${port}/`)}, but ONLY if you've set up port forwarding on your router`)
     }
-    console.log(
-      colours.yellow(
-        `Plain http requests will be served using files from the static/ folder, which you can access in your browser at ${colours.blue(`http://localhost:${port}/`)}.`,
-      ),
-    );
-    console.log(colours.red("Press control+C to stop the server."));
-  });
+    console.log(colours.yellow(`Any route that is not websocket will be servced from static/ folder, which you can access in your browser at ${colours.blue(`http://localhost:${port}/`)}.`))
+    console.log(colours.red('Press Control+C to stop the server.'))
+  })
 }
 
-module.exports = startServer;
+module.exports = startServer
